65ba233df3a37eb391263b7948a33c028f22d0e7
PepaHub <nickminaylov@gmail.com>
2025-08-05 15:05:40 +0300

    Delete calculator directory

--- calculator/main.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.result').style.display = 'none';
-
-document.getElementById('calculate-btn').addEventListener('click', function () {
-    document.querySelector('.result').style.display = 'block';
-});
